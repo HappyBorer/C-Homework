d9cb6ede30c5eac49c2ab665b77e9e951c21276e
Ursuzev Boris <BorerKicker@yandex.ru>
2022-06-07 21:18:10 +0300

    added additional task 2

--- Homeworks first seminar/additional task 2/Program.cs
+++ Homeworks first seminar/additional task 2/Program.cs
@@ -1,2 +1,10 @@
 ﻿/*Есть программа с бесконечным циклом. Когда пользователь
 вводит exit программа заканчивается*/
+
+Console.Write("Ввод: ");
+string line = Console.ReadLine();
+while(line != "exit")
+{
+    Console.Write("Ввод: ");
+    line = Console.ReadLine();
+}
--- Homeworks first seminar/task 2/Program.cs
+++ Homeworks first seminar/task 2/Program.cs
@@ -7,7 +7,7 @@ Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третие число: ");
+Console.Write("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
 if(max < number2)
