b5a9a2d3b1fb4849ec4d9cc0c54fdce0c7af6060
Ursuzev Boris <BorerKicker@yandex.ru>
2022-06-07 18:27:22 +0300

    added additional task 1

---
+++ Homeworks first seminar/additional task 1/Program.cs
@@ -0,0 +1,28 @@
+﻿ /*В переменной string есть секретное сообщение, во второй есть пароль. 
+ Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.*/
+string classifiedMessage = "Купил слона";
+string password = "СЛОН";
+string inputPassword = "";
+int attempt = 3;
+int count = 1;
+Console.WriteLine("Он трубит, но не трубач,");
+Console.WriteLine("Всеми признанный силач.");
+Console.WriteLine("Стоя спит под пальмой он.");
+while(count <= attempt && inputPassword != password)
+{
+    Console.Write(count + " попытка ввода пароля: ");
+    inputPassword =  Console.ReadLine();
+    if(inputPassword == password)
+    {
+        Console.WriteLine(classifiedMessage);
+    }
+    else
+    {
+        Console.WriteLine("Неверный пароль");
+    }
+    count++;
+}
+if (count > attempt)
+{
+    Console.WriteLine("Попытки закончились");
+}
