3eb752432ee87f9a0f81fd2cf48c9eb6f2663265
Ursuzev Boris <BorerKicker@yandex.ru>
2022-06-10 18:02:41 +0300

    Added additional task

---
+++ Homeworks second seminar/additional task/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Игра угадайка. Программа загадывает случайное число. Пользователь его угадывает. 
+Если пользователь дает неправильный ответ, то программа сообщает, больше загаданное
+число или меньше. На отгадывание дается 3 попытки. */
+
+Console.Clear();
+void NumberCompar(int numGue, int tr)
+{
+    while(tr > 0)
+    {   
+        Console.Write($"Осталось попыток {tr}: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > numGue)
+        {
+            Console.WriteLine("Неправильно. Угадываемое число меньше!");
+        }
+        else if (number < numGue)
+        {
+            Console.WriteLine("Неправильно. Угадываемое число больше!");
+        }
+        else 
+        {
+            Console.WriteLine("Ты угадал!!!");
+            break;
+        }   
+        tr--;
+        if (tr == 0)
+        {
+            Console.WriteLine("Попытки кончились.");
+        }
+}
+}
+int numberGuess = new Random().Next(1, 1000);
+Console.WriteLine("Угадай число от 1 до 1000");
+int attempt = 3;
+NumberCompar(numberGuess, attempt);
