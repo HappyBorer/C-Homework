c1154fc5bd68b1a2ac660d2a2c5e2e1b9b144cf4
Ursuzev Boris <BorerKicker@yandex.ru>
2022-06-10 10:50:24 +0300

    added task 1 of the second seminar

---
+++ Homeworks second seminar/task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99 && number < 1000)
+{
+    int figure2 = number % 100 / 10;
+    Console.WriteLine(figure2);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное!");
+}
