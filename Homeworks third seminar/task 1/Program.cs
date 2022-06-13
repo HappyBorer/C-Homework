dcc15c820df81ee679abffd047edf99394e8fa8f
Ursuzev Boris <BorerKicker@yandex.ru>
2022-06-13 22:14:19 +0300

    Added tesk 1 third seminar

---
+++ Homeworks third seminar/task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да */
+
+using static System.Console;
+Clear();
+Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(ReadLine());
+if (number < 10000 && number > 99999)
+{
+    WriteLine("Ошибка! Чиcло не пятизначное!");
+    return;
+}
+int NumberFlip(int num)
+{
+    string figure = " ";
+    while(num > 0)
+    {
+        int result = num % 10;
+        figure += Convert.ToString(result);
+        num /= 10;
+
+    }
+    return Convert.ToInt32(figure);
+}
+if (number == NumberFlip(number))
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
