70905834a34b3493db5c35420aaa20f0d559b731
Boris <BorerKicker@yandex.ru>
2022-06-21 14:56:22 +0300

    Added the task of the 1st fifth seminsar

---
+++ Homework fifth seminar/task 1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+Console.Clear();
+int size = 6;
+int[] arrayIntegers = new int[size];
+
+int[] FillArrayRandom(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+void SearchEvenNembersArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            count++;
+        } 
+    }
+    Console.WriteLine($"Чётных чисел в массиве = {count}");
+}
+arrayIntegers = FillArrayRandom(arrayIntegers);
+SearchEvenNembersArray(arrayIntegers);
+
