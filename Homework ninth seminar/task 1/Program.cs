ab7fc0249cd97705af59207d85f638864bfc6c95
Ursuzev Boris <BorerKicker@yandex.ru>
2022-07-05 15:36:12 +0300

    added task 1 of the ninth seminar

---
+++ Homework ninth seminar/task 1/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+// Методы
+using static System.Console;
+Clear();
+
+
+int InputNumber(string output)
+{
+    Write(output);
+    return Convert.ToInt32(ReadLine());
+}
+
+void Backing(int number)
+{
+    if (number > 1)
+    {
+        Console.Write($"{number}, ");
+        Backing(number - 1);
+    }
+    else
+    {
+        Console.Write($"{number} ");
+    }
+}
+
+
+// Мой код
+
+int number;
+while (true)
+{
+    number = InputNumber("Введите натуральное число: ");
+    if (number < 1) WriteLine("Число не натуральное!");
+    else break;
+}
+Backing(number);
