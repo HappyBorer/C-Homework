5d5a9e15ffc5cd85bba91b4a7f332ac3f94ef0e8
Ursuzev Boris <BorerKicker@yandex.ru>
2022-07-06 22:25:52 +0300

    added additional task of the ninth seminar

---
+++ Homework ninth seminar/additional task/Program.cs
@@ -0,0 +1,87 @@
+﻿/* Console.SetCursorPosition() - позволяет поставить курсор в какую-либо точку консоли
+Console.CursorVisible - изменяет видимость курсора
+Console.ReadKey() - ожидает нажатия клавиши на клавиатуре и возвращает ConsoleKeyInfo
+В ConsoleKeyInfo.Key можно получить ConsoleKey - это нажатая пользователем клавиша
+
+Задача написать игру-бродилку, где можно управлять персонажем стрелками вверх-вниз-влево-вправо. 
+Область движения ограничена стенками ('*'), персонаж обозначается символом '@'. Область можно задать 
+через двумерный массив. Персонаж должен иметь свободу движения в пределах его области. */
+
+
+using static System.Console;
+Clear();
+// Методы
+
+
+
+// Мой код
+ConsoleKeyInfo key;
+int sizeRows = 19;
+int sizeColumns = 10;
+char person = '@';
+int positionPersonX = 9;
+int positionPersonY = 5;
+WriteLine("Используйте для управления стрелочки на клавиатуре. Для выхода нажмите Escape");
+WriteLine("Для продолжения нажмите любую клавишу.");
+ReadKey();
+Clear();
+Console.SetCursorPosition(positionPersonX, positionPersonY);
+Write(person);
+while (true)
+{
+    Console.CursorVisible = false;
+    for (int i = 0; i < sizeRows; i++)
+    {
+        for (int j = 0; j < sizeColumns; j++)
+        {
+            if (i == 0 || i == sizeRows - 1 || j == 0 || j == sizeColumns - 1)
+            {
+                Console.SetCursorPosition(i, j);
+                Write('*');
+            }
+
+
+        }
+
+    }
+    //Write(' ');
+
+    Console.SetCursorPosition(positionPersonX, positionPersonY);
+    key = ReadKey();
+
+    if (key.Key == ConsoleKey.UpArrow && positionPersonY > 1)
+    {
+        Write(' ');
+        positionPersonY--;
+        Console.SetCursorPosition(positionPersonX, positionPersonY);
+    }
+    else if (key.Key == ConsoleKey.DownArrow && positionPersonY < sizeColumns - 2)
+    {
+        Write(' ');
+        positionPersonY++;
+        Console.SetCursorPosition(positionPersonX, positionPersonY);
+    }
+    else if (key.Key == ConsoleKey.LeftArrow && positionPersonX > 1)
+    {
+        Write(' ');
+        positionPersonX--;
+        Console.SetCursorPosition(positionPersonX, positionPersonY);
+    }
+    else if (key.Key == ConsoleKey.RightArrow && positionPersonX < sizeRows - 2)
+    {
+        Write(' ');
+        positionPersonX++;
+        Console.SetCursorPosition(positionPersonX, positionPersonY);
+    }
+    else if (key.Key == ConsoleKey.Escape)
+    {
+        Clear();
+        break;
+    }
+
+    Write(person);
+
+}
+
+
+
